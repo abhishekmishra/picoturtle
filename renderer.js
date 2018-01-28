037bd6629aeed96e643710460341bc151c9f2ae2
Abhishek Mishra <abhishekmishra3@yahoo.com>
2018-01-28 23:11:05 +0530

    added code to init canvas and add required packages bootstrap and jquery

---
+++ renderer.js
@@ -0,0 +1,9 @@
+// This file is required by the index.html file and will
+// be executed in the renderer process for that window.
+// All of the Node.js APIs are available in this process.
+
+console.log('we are in the index window file');
+require('bootstrap');
+
+let Turtle = require('./src/turtle');
+let t = new Turtle('turtleCanvas');
---
+++ src/turtle.js
@@ -0,0 +1,35 @@
+let $ = require('jQuery');
+
+class Turtle {
+
+    constructor(canvas_id, name = 'picoturtle') {
+        this.canvas_id = canvas_id;
+        this.name = name;
+        this.initCanvas();
+        this.initTurtle();
+    }
+
+    initCanvas() {
+        console.log(this.canvas_id);
+        this.canvas = $('#' + this.canvas_id)[0];
+        console.log(this.canvas);
+        this.width = this.canvas.width;
+        this.height = this.canvas.height;
+        this.ctx = this.canvas.getContext('2d');
+    }
+
+    initTurtle() {
+        let c = new Point(this.width/2, this.height/2);
+        this.ctx.fillStyle = 'rgb(200,0,0)';
+        this.ctx.fillRect(c.x, c.y, 10, 10);
+    }
+}
+
+class Point {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+module.exports = Turtle;
