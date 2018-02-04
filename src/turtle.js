182f829af8136766084a3819d09105e252982793
Abhishek Mishra <abhishekmishra3@yahoo.com>
2018-02-04 23:12:16 +0530

    added turtle as a triangle with one end facing the orientation of the turtle

--- src/turtle.js
+++ src/turtle.js
@@ -39,7 +39,6 @@ class Turtle {
     }
 
     forward(d) {
-        let slope = Math.tan(this.angle * Math.PI / 180);
         let theta = this.angle * Math.PI / 180;
         // y2 = d sin (theta) + y1
         // x2 = d cos (theta) + x1 
@@ -89,31 +88,51 @@ class Turtle {
     }
 
     drawTurtle() {
+        this.ctx.save();
         let color = this.ctx.strokeStyle;
         this.ctx.strokeStyle = 'black';
         this.ctx.fillStyle = 'black';
-        this.ctx.fillRect(this.location.x - 5, this.location.y - 5, 10, 10);
+        this.ctx.lineWidth = 3;
+        // this.ctx.fillRect(this.location.x - 5, this.location.y - 5, 10, 10);
+
+        let d = 25;
+        let theta1 = (this.angle - 145) * Math.PI / 180;
+        let y2 = d * (Math.sin(theta1)) + this.location.y;
+        let x2 = d * (Math.cos(theta1)) + this.location.x;
+
+        let theta2 = (this.angle + 145) * Math.PI / 180;
+        let y3 = d * (Math.sin(theta2)) + this.location.y;
+        let x3 = d * (Math.cos(theta2)) + this.location.x;
+
+        this.ctx.beginPath();
+        this.ctx.moveTo(this.location.x, this.location.y);
+        this.ctx.lineTo(x2, y2);
+        this.ctx.lineTo(x3, y3);
+        this.ctx.lineTo(this.location.x, this.location.y);
+        this.ctx.stroke();
+
         this.color(color);
+        this.ctx.restore();
     }
 
     exec() {
         if (arguments.length > 0) {
             this.clear();
 
-            for(let i = 0; i < this.history.length; i++) {
+            for (let i = 0; i < this.history.length; i++) {
                 var cmdArgs = this.history[i];
-                if(cmdArgs.length == 1) {
+                if (cmdArgs.length == 1) {
                     this[cmdArgs[0]]();
-                } else if(cmdArgs.length == 2) {
+                } else if (cmdArgs.length == 2) {
                     this[cmdArgs[0]](cmdArgs[1]);
                 }
             }
 
             let command = arguments[0];
             this.addToHistory(arguments);
-            if(arguments.length == 1) {
+            if (arguments.length == 1) {
                 this[command]();
-            } else if(arguments.length == 2) {
+            } else if (arguments.length == 2) {
                 this[command](arguments[1]);
             }
 
