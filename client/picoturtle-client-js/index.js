9c9b2a59b1faab3e5cf087d28d21f559c62aeee5
Abhishek Mishra <abhishekmishra3@gmail.com>
2019-02-21 20:24:30 +0530

    added static code as separate js package

---
+++ client/picoturtle-client-js/index.js
@@ -0,0 +1,4 @@
+import { Turtle, Point } from './turtle_canvas.js';
+
+module.exports.Turtle = Turtle;
+module.exports.Point = Point;
--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'node_modules/picoturtle-client-js')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
