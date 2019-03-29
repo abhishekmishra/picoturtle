a390d90958dfcacc32af6ecd411f6ec337981142
Abhishek Mishra <abhishekmishra3@gmail.com>
2019-03-29 15:15:24 +0530

    added handlers to get/set to prefs

--- preferences.js
+++ preferences.js
@@ -1,3 +1,4 @@
+const { dialog } = require('electron').remote;
 const Store = require('electron-store');
 const setTheme = require('./utils').setTheme;
 const preferenceConfig = require('./utils').preferenceConfig;
@@ -44,18 +45,74 @@ $('#pref_appearance').click();
 function showPage(item) {
     $('#prefitems').html('');
     for (var x in store.get(item)) {
+        let store_name = item + '.' + x;
+        let id = 'pref_' + item + '_' + x;
         let label = x;
+        let type = 'text';
         if (x in preferenceConfig) {
             label = preferenceConfig[x].label;
+            type = preferenceConfig[x].type;
         }
-        $('#prefitems').append(
-            '<a href="#" class="list-group-item list-group-item-dark">'
+
+        let item_content = '<div class="list-group-item list-group-item-dark">'
             + '<div class="d-flex w-100 justify-content-between">'
-            + label
-            + '<input type="text" class="w-100 ml-3"></input>'
+            // + '<form>'
+            + '<div class="form-group row w-100">'
+            + '<label for="staticEmail" class="col-sm-3 col-form-label text-right">' + label + '</label>'
+            + '<div class="col-sm-9">';
+
+        if (type == 'text') {
+            item_content += '<input type="text" id="' + id + '" class="w-100 ml-3"></input>';
+        }
+        if (type == 'select') {
+            let options = '';
+            for (var i = 0; i < preferenceConfig[x].options.length; i++) {
+                // console.log(preferenceConfig[x].options[i]);
+                options += '<option value="'
+                    + preferenceConfig[x].options[i].value
+                    + '">'
+                    + preferenceConfig[x].options[i].key
+                    + '</option>';
+            }
+            item_content += '<select id="' + id + '" class="form-control">'
+                + options
+                + '</select>';
+        }
+        if (type == 'path') {
+            item_content += '<div class="form-group row w-100">'
+                + '<div class="col-sm-8 m-0 p-1"><input type="text" id="' + id + '" class="form-control mb-2"></input></div>'
+                + '<div class="col-sm-4 m-0 p-1">'
+                + '<button id="browse_' + id + '"class="btn btn-secondary btn-sm mb-2 form-control">Browse</button></div>'
+                + '</div>';
+        }
+        item_content += '</div>'
+            // + '</form>'
             + '</div>'
-            + '</a>'
-        );
+            + '</div>';
+
+        //add the generated content
+        $('#prefitems').append(item_content);
+
+        //set the current value
+        $('#' + id).val(store.get(store_name));
+
+        //listen for change value
+        $('#' + id).on('change', (event) => {
+            console.log('setting ' + store_name + ' as ' + event.target.value);
+            store.set(store_name, event.target.value);
+        });
+
+        //add browse handler if type is path
+        if (type == "path") {
+            $('#browse_' + id).on('click', (event) => {
+                var selectedPath = dialog.showOpenDialog({ properties: ['openFile'] });
+                if (selectedPath !== undefined) {
+                    console.log(selectedPath);
+                    $('#' + id).val(selectedPath);
+                    $('#' + id).change();
+                }
+            });
+        }
     }
 }
 
