2d167f741fc29355b0a151884df43fbea67fa80f
Abhishek Mishra <abhishekmishra3@gmail.com>
2022-03-09 21:51:00 +0530

    lua is added to lua path if env. var not provided

--- src/TurtleApp.cpp
+++ src/TurtleApp.cpp
@@ -188,20 +188,18 @@ int initTurtleLuaBinding(lua_State **luaState)
     if (turtleLuaDir == NULL || strlen(turtleLuaDir) == 0)
     {
         printf("Warning: %s is not set or empty!\n", TURTLE_LUA_DIR_ENV_VAR);
+        turtleLuaDir = "lua";
     }
-    else
+    char *setPathCodeStr = (char *)calloc(strlen(turtleLuaDir) + 1024, sizeof(char));
+    if (setPathCodeStr == NULL)
     {
-        char *setPathCodeStr = (char *)calloc(strlen(turtleLuaDir) + 1024, sizeof(char));
-        if (setPathCodeStr == NULL)
-        {
-            printf("Fatal: Unable to alloc string to set load path in lua!\n");
-        }
-
-        sprintf(setPathCodeStr, "package.path = '%s/?.lua;' .. package.path", turtleLuaDir);
-        // printf("Setting path via code -> |%s|\n", setPathCodeStr);
-
-        runLuaScript(L, setPathCodeStr);
+        printf("Fatal: Unable to alloc string to set load path in lua!\n");
     }
+
+    sprintf(setPathCodeStr, "package.path = '%s/?.lua;' .. package.path", turtleLuaDir);
+    // printf("Setting path via code -> |%s|\n", setPathCodeStr);
+
+    runLuaScript(L, setPathCodeStr);
     return 0;
 }
 
