909b284bb382b69a52bee8e5518d2c86c801f0f6
Abhishek Mishra <abhishekmishra3@gmail.com>
2022-03-10 09:24:36 +0530

    add current folder to lua path, move hexdump lower in file

--- src/TurtleApp.cpp
+++ src/TurtleApp.cpp
@@ -22,14 +22,6 @@ void turtleInitCb(turtle::PicoTurtle *t, void *cb_args);
 void turtleDestroyCb(turtle::PicoTurtle *t, void *cb_args);
 int gui_window();
 
-static const char *const usages[] = {
-    "picoturtle [options] [[--] <turtle file> <turtle args>]",
-    "picoturtle [options]",
-    NULL,
-};
-
-sk_sp<SkImage> img;
-
 // https://stackoverflow.com/a/7776146/9483968
 // Usage:
 //     hexDump(desc, addr, len, perLine);
@@ -41,78 +33,15 @@ void hexDump(
     const char *desc,
     const void *addr,
     const int len,
-    int perLine)
-{
-    // Silently ignore silly per-line values.
+    int perLine);
 
-    if (perLine < 4 || perLine > 64)
-        perLine = 16;
+static const char *const usages[] = {
+    "picoturtle [options] [[--] <turtle file> <turtle args>]",
+    "picoturtle [options]",
+    NULL,
+};
 
-    int i;
-    unsigned char *buff = (unsigned char *)calloc(perLine + 1, sizeof(unsigned char));
-    const unsigned char *pc = (const unsigned char *)addr;
-
-    // Output description if given.
-
-    if (desc != NULL)
-        printf("%s:\n", desc);
-
-    // Length checks.
-
-    if (len == 0)
-    {
-        printf("  ZERO LENGTH\n");
-        return;
-    }
-    if (len < 0)
-    {
-        printf("  NEGATIVE LENGTH: %d\n", len);
-        return;
-    }
-
-    // Process every byte in the data.
-
-    for (i = 0; i < len; i++)
-    {
-        // Multiple of perLine means new or first line (with line offset).
-
-        if ((i % perLine) == 0)
-        {
-            // Only print previous-line ASCII buffer for lines beyond first.
-
-            if (i != 0)
-                printf("  %s\n", buff);
-
-            // Output the offset of current line.
-
-            printf("  %04x ", i);
-        }
-
-        // Now the hex code for the specific character.
-
-        printf(" %02x", pc[i]);
-
-        // And buffer a printable ASCII character for later.
-
-        if ((pc[i] < 0x20) || (pc[i] > 0x7e)) // isprint() may be better.
-            buff[i % perLine] = '.';
-        else
-            buff[i % perLine] = pc[i];
-        buff[(i % perLine) + 1] = '\0';
-    }
-
-    // Pad out last line if not exactly perLine characters.
-
-    while ((i % perLine) != 0)
-    {
-        printf("   ");
-        i++;
-    }
-
-    // And print the final ASCII buffer.
-
-    printf("  %s\n", buff);
-}
+sk_sp<SkImage> img;
 
 int main(int argc, char *argv[])
 {
@@ -196,7 +125,7 @@ int initTurtleLuaBinding(lua_State **luaState)
         printf("Fatal: Unable to alloc string to set load path in lua!\n");
     }
 
-    sprintf(setPathCodeStr, "package.path = '%s/?.lua;' .. package.path", turtleLuaDir);
+    sprintf(setPathCodeStr, "package.path = '%s/?.lua;?.lua;' .. package.path", turtleLuaDir);
     // printf("Setting path via code -> |%s|\n", setPathCodeStr);
 
     runLuaScript(L, setPathCodeStr);
@@ -368,6 +297,83 @@ int gui_window()
     }
 
     return 0;
+}
+
+void hexDump(
+    const char *desc,
+    const void *addr,
+    const int len,
+    int perLine)
+{
+    // Silently ignore silly per-line values.
+
+    if (perLine < 4 || perLine > 64)
+        perLine = 16;
+
+    int i;
+    unsigned char *buff = (unsigned char *)calloc(perLine + 1, sizeof(unsigned char));
+    const unsigned char *pc = (const unsigned char *)addr;
+
+    // Output description if given.
+
+    if (desc != NULL)
+        printf("%s:\n", desc);
+
+    // Length checks.
+
+    if (len == 0)
+    {
+        printf("  ZERO LENGTH\n");
+        return;
+    }
+    if (len < 0)
+    {
+        printf("  NEGATIVE LENGTH: %d\n", len);
+        return;
+    }
+
+    // Process every byte in the data.
+
+    for (i = 0; i < len; i++)
+    {
+        // Multiple of perLine means new or first line (with line offset).
+
+        if ((i % perLine) == 0)
+        {
+            // Only print previous-line ASCII buffer for lines beyond first.
+
+            if (i != 0)
+                printf("  %s\n", buff);
+
+            // Output the offset of current line.
+
+            printf("  %04x ", i);
+        }
+
+        // Now the hex code for the specific character.
+
+        printf(" %02x", pc[i]);
+
+        // And buffer a printable ASCII character for later.
+
+        if ((pc[i] < 0x20) || (pc[i] > 0x7e)) // isprint() may be better.
+            buff[i % perLine] = '.';
+        else
+            buff[i % perLine] = pc[i];
+        buff[(i % perLine) + 1] = '\0';
+    }
+
+    // Pad out last line if not exactly perLine characters.
+
+    while ((i % perLine) != 0)
+    {
+        printf("   ");
+        i++;
+    }
+
+    // And print the final ASCII buffer.
+
+    printf("  %s\n", buff);
 }
 
 // void turtle_example()
