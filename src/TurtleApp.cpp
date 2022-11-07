d95a8a18bb5f3352b477e9046600f14a696ab12d
Abhishek Mishra <abhishekmishra3@gmail.com>
2022-11-07 11:38:02 +0530

    renamed MainWindow to TurtleAppWindow

--- src/TurtleApp.cpp
+++ src/TurtleApp.cpp
@@ -19,284 +19,297 @@
 
 #define TURTLE_LUA_DIR_ENV_VAR "TURTLE_LUA_DIR"
 
-int initTurtleLuaBinding(lua_State **luaState);
-int handleTurtleLuaArgs(lua_State *L, int argc, char *argv[]);
-int runLuaFile(lua_State *luaState, const char *filename);
-int runLuaScript(lua_State *luaState, const char *script);
-void cleanupTurtleLuaBinding(lua_State *luaState);
-void turtleInitCb(turtle::PicoTurtle *t, void *cb_args);
-void turtleDestroyCb(turtle::PicoTurtle *t, void *cb_args);
-int gui_window();
+int init_turtle_lua_binding(lua_State** luaState);
+int handle_turtle_lua_args(lua_State* L, int argc, char* argv[]);
+int run_lua_file(lua_State* luaState, const char* filename);
+int run_lua_script(lua_State* luaState, const char* script);
+void cleanup_turtle_lua_binding(lua_State* luaState);
+void turtle_init_cb(turtle::PicoTurtle* t, void* cb_args);
+void turtle_destroy_cb(turtle::PicoTurtle* t, void* cb_args);
+
+/**
+* Creates the Qt main window and runs the main loop for Qt.
+* 
+* @param argc program args
+* @param argv program args
+* @return return value from qt mainloop
+*/
+int gui_window(int argc, char* argv[]);
 
 // https://stackoverflow.com/a/7776146/9483968
 // Usage:
-//     hexDump(desc, addr, len, perLine);
+//     hex_dump(desc, addr, len, perLine);
 //         desc:    if non-NULL, printed as a description before hex dump.
 //         addr:    the address to start dumping from.
 //         len:     the number of bytes to dump.
 //         perLine: number of bytes on each output line.
-void hexDump(
-    const char *desc,
-    const void *addr,
-    const int len,
-    int perLine);
+void hex_dump(
+	const char* desc,
+	const void* addr,
+	const int len,
+	int perLine);
 
-static const char *const usages[] = {
-    "picoturtle [options] [[--] <turtle file> <turtle args>]",
-    "picoturtle [options]",
-    NULL,
+static const char* const usages[] = {
+	"picoturtle [options] [[--] <turtle file> <turtle args>]",
+	"picoturtle [options]",
+	NULL,
 };
 
 sk_sp<SkImage> img;
 
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
-    // lua state object (see lua api docs)
-    lua_State *L;
+	// lua state object (see lua api docs)
+	lua_State* L;
 
-    // option to indicate if app is to run in gui mode
-    int gui = 0;
+	// option to indicate if app is to run in gui mode
+	int gui = 0;
 
-    struct argparse_option options[] = {
-        OPT_HELP(),
-        OPT_GROUP("PicoTurtle Options"),
-        OPT_BOOLEAN('g', "gui", &gui, "run in gui mode.", NULL, 0, 0),
-        OPT_END(),
-    };
+	struct argparse_option options[] = {
+		OPT_HELP(),
+		OPT_GROUP("PicoTurtle Options"),
+		OPT_BOOLEAN('g', "gui", &gui, "run in gui mode.", NULL, 0, 0),
+		OPT_END(),
+	};
 
-    struct argparse argparse;
-    argparse_init(&argparse, options, usages, 0);
-    argparse_describe(&argparse, "\npicoturtle: A turtle programming environment.", "\nTODO: more info.");
-    argc = argparse_parse(&argparse, argc, (const char **)argv);
+	struct argparse argparse;
+	argparse_init(&argparse, options, usages, 0);
+	argparse_describe(&argparse, "\npicoturtle: A turtle programming environment.", "\nTODO: more info.");
+	argc = argparse_parse(&argparse, argc, (const char**)argv);
 
-    turtle::PicoTurtle::set_init_callback(&turtleInitCb, NULL);
-    turtle::PicoTurtle::set_destroy_callback(&turtleDestroyCb, NULL);
+	turtle::PicoTurtle::set_init_callback(&turtle_init_cb, NULL);
+	turtle::PicoTurtle::set_destroy_callback(&turtle_destroy_cb, NULL);
 
-    // initialize the turtle lua binding with args
-    initTurtleLuaBinding(&L);
-    handleTurtleLuaArgs(L, argc, argv);
-    // cleanup the turtle lua binding
-    cleanupTurtleLuaBinding(L);
+	// initialize the turtle lua binding with args
+	init_turtle_lua_binding(&L);
+	handle_turtle_lua_args(L, argc, argv);
+	// cleanup the turtle lua binding
+	cleanup_turtle_lua_binding(L);
 
-    if (gui != 0)
-    {
-        QApplication a(argc, argv);
-        TurtleAppWindow mainWindow;
-        mainWindow.show();
-        return a.exec();
-    }
+	if (gui != 0)
+	{
+		gui_window(argc, argv);
+	}
 
-    return 0;
+	return 0;
 }
 
-bool handleLuaError(lua_State *luaState, int luaErrorCode)
+int gui_window(int argc, char* argv[])
 {
-    if (luaErrorCode == LUA_OK)
-    {
-        return true;
-    }
-    else
-    {
-        const char *err_msg = lua_tostring(luaState, -1);
-        std::cout << err_msg << std::endl;
-        return false;
-    }
+	QApplication a(argc, argv);
+	turtle::TurtleAppWindow mainWindow;
+	mainWindow.show();
+	return a.exec();
 }
 
-int initTurtleLuaBinding(lua_State **luaState)
+bool handleLuaError(lua_State* luaState, int luaErrorCode)
 {
-    // Create Lua State
-    (*luaState) = luaL_newstate();
-    lua_State *L = (*luaState);
-
-    if (L == NULL)
-    {
-        return -1;
-    }
-
-    // Add standard libraries to Lua Virtual Machine
-    luaL_openlibs(L);
-
-    // picoturtle = require "picoturtle"
-    luaL_requiref(L, "picoturtle", luaopen_picoturtle, 1);
-    lua_pop(L, 1); /* remove result from previous call */
-
-    // TODO: Set path using optional args
-    char *turtleLuaDir = getenv(TURTLE_LUA_DIR_ENV_VAR);
-    if (turtleLuaDir == NULL || strlen(turtleLuaDir) == 0)
-    {
-        printf("Warning: %s is not set or empty!\n", TURTLE_LUA_DIR_ENV_VAR);
-        turtleLuaDir = (char *)"lua";
-    }
-    char *setPathCodeStr = (char *)calloc(strlen(turtleLuaDir) + 1024, sizeof(char));
-    if (setPathCodeStr == NULL)
-    {
-        printf("Fatal: Unable to alloc string to set load path in lua!\n");
-    }
-
-    sprintf(setPathCodeStr, "package.path = '%s/?.lua;?.lua;' .. package.path", turtleLuaDir);
-    // printf("Setting path via code -> |%s|\n", setPathCodeStr);
-
-    runLuaScript(L, setPathCodeStr);
-    return 0;
+	if (luaErrorCode == LUA_OK)
+	{
+		return true;
+	}
+	else
+	{
+		const char* err_msg = lua_tostring(luaState, -1);
+		std::cout << err_msg << std::endl;
+		return false;
+	}
 }
 
-int handleTurtleLuaArgs(lua_State *L, int argc, char *argv[])
+int init_turtle_lua_binding(lua_State** luaState)
 {
-    const char *fname = NULL;
+	// Create Lua State
+	(*luaState) = luaL_newstate();
+	lua_State* L = (*luaState);
 
-    lua_newtable(L);
-    if (argc > 0)
-    {
-        fname = argv[0];
-        if (argc > 1)
-        {
-            for (int i = 1; i < argc; i++)
-            {
-                lua_pushstring(L, argv[i]);
-                lua_seti(L, -2, i - 1);
-            }
-        }
-        printf("input lua file - %s\n", fname);
-    }
-    lua_setglobal(L, "arg");
+	if (L == NULL)
+	{
+		return -1;
+	}
 
-    if (fname != NULL)
-    {
-        runLuaFile(L, fname);
-    }
+	// Add standard libraries to Lua Virtual Machine
+	luaL_openlibs(L);
 
-    return 0;
+	// picoturtle = require "picoturtle"
+	luaL_requiref(L, "picoturtle", luaopen_picoturtle, 1);
+	lua_pop(L, 1); /* remove result from previous call */
+
+	// TODO: Set path using optional args
+	char* turtleLuaDir = getenv(TURTLE_LUA_DIR_ENV_VAR);
+	if (turtleLuaDir == NULL || strlen(turtleLuaDir) == 0)
+	{
+		printf("Warning: %s is not set or empty!\n", TURTLE_LUA_DIR_ENV_VAR);
+		turtleLuaDir = (char*)"lua";
+	}
+	char* setPathCodeStr = (char*)calloc(strlen(turtleLuaDir) + 1024, sizeof(char));
+	if (setPathCodeStr == NULL)
+	{
+		printf("Fatal: Unable to alloc string to set load path in lua!\n");
+	}
+
+	sprintf(setPathCodeStr, "package.path = '%s/?.lua;?.lua;' .. package.path", turtleLuaDir);
+	// printf("Setting path via code -> |%s|\n", setPathCodeStr);
+
+	run_lua_script(L, setPathCodeStr);
+	return 0;
 }
 
-int runLuaFile(lua_State *luaState, const char *filename)
+int handle_turtle_lua_args(lua_State* L, int argc, char* argv[])
 {
-    if (filename != NULL && strlen(filename) > 0)
-    {
-        if (handleLuaError(luaState, luaL_dofile(luaState, filename)))
-        {
-            printf("File execution complete -> %s. \n", filename);
-        }
-        return 0;
-    }
-    else
-    {
-        return -1;
-    }
+	const char* fname = NULL;
+
+	lua_newtable(L);
+	if (argc > 0)
+	{
+		fname = argv[0];
+		if (argc > 1)
+		{
+			for (int i = 1; i < argc; i++)
+			{
+				lua_pushstring(L, argv[i]);
+				lua_seti(L, -2, i - 1);
+			}
+		}
+		printf("input lua file - %s\n", fname);
+	}
+	lua_setglobal(L, "arg");
+
+	if (fname != NULL)
+	{
+		run_lua_file(L, fname);
+	}
+
+	return 0;
 }
 
-int runLuaScript(lua_State *luaState, const char *script)
+int run_lua_file(lua_State* luaState, const char* filename)
 {
-    if (script != NULL && strlen(script) > 0)
-    {
-        if (handleLuaError(luaState, luaL_dostring(luaState, script)))
-        {
-            // uncomment for debug only.
-            // printf("Script execution complete.\n");
-        }
-        return 0;
-    }
-    else
-    {
-        return -1;
-    }
+	if (filename != NULL && strlen(filename) > 0)
+	{
+		if (handleLuaError(luaState, luaL_dofile(luaState, filename)))
+		{
+			printf("File execution complete -> %s. \n", filename);
+		}
+		return 0;
+	}
+	else
+	{
+		return -1;
+	}
 }
 
-void cleanupTurtleLuaBinding(lua_State *luaState)
+int run_lua_script(lua_State* luaState, const char* script)
 {
-    lua_close(luaState);
+	if (script != NULL && strlen(script) > 0)
+	{
+		if (handleLuaError(luaState, luaL_dostring(luaState, script)))
+		{
+			// uncomment for debug only.
+			// printf("Script execution complete.\n");
+		}
+		return 0;
+	}
+	else
+	{
+		return -1;
+	}
 }
 
-void turtleInitCb(turtle::PicoTurtle *t, void *cb_args)
+void cleanup_turtle_lua_binding(lua_State* luaState)
 {
-    printf("PicoTurtle created - Name: %s, Id: %s\n", t->getName().c_str(), t->getId().c_str());
+	lua_close(luaState);
 }
 
-void turtleDestroyCb(turtle::PicoTurtle *t, void *cb_args)
+void turtle_init_cb(turtle::PicoTurtle* t, void* cb_args)
 {
-    printf("PicoTurtle destroyed - Name: %s, Id: %s\n", t->getName().c_str(), t->getId().c_str());
-    img = t->getRasterSurface()->makeImageSnapshot();
-    if (img)
-    {
-        printf("Image [%d x %d].\n", img->width(), img->height());
-    }
+	printf("PicoTurtle created - Name: %s, Id: %s\n", t->getName().c_str(), t->getId().c_str());
 }
 
-void hexDump(
-    const char *desc,
-    const void *addr,
-    const int len,
-    int perLine)
+void turtle_destroy_cb(turtle::PicoTurtle* t, void* cb_args)
 {
-    // Silently ignore silly per-line values.
+	printf("PicoTurtle destroyed - Name: %s, Id: %s\n", t->getName().c_str(), t->getId().c_str());
+	img = t->getRasterSurface()->makeImageSnapshot();
+	if (img)
+	{
+		printf("Image [%d x %d].\n", img->width(), img->height());
+	}
+}
 
-    if (perLine < 4 || perLine > 64)
-        perLine = 16;
+void hex_dump(
+	const char* desc,
+	const void* addr,
+	const int len,
+	int perLine)
+{
+	// Silently ignore silly per-line values.
 
-    int i;
-    unsigned char *buff = (unsigned char *)calloc(perLine + 1, sizeof(unsigned char));
-    const unsigned char *pc = (const unsigned char *)addr;
+	if (perLine < 4 || perLine > 64)
+		perLine = 16;
 
-    // Output description if given.
+	int i;
+	unsigned char* buff = (unsigned char*)calloc(perLine + 1, sizeof(unsigned char));
+	const unsigned char* pc = (const unsigned char*)addr;
 
-    if (desc != NULL)
-        printf("%s:\n", desc);
+	// Output description if given.
 
-    // Length checks.
+	if (desc != NULL)
+		printf("%s:\n", desc);
 
-    if (len == 0)
-    {
-        printf("  ZERO LENGTH\n");
-        return;
-    }
-    if (len < 0)
-    {
-        printf("  NEGATIVE LENGTH: %d\n", len);
-        return;
-    }
+	// Length checks.
 
-    // Process every byte in the data.
+	if (len == 0)
+	{
+		printf("  ZERO LENGTH\n");
+		return;
+	}
+	if (len < 0)
+	{
+		printf("  NEGATIVE LENGTH: %d\n", len);
+		return;
+	}
 
-    for (i = 0; i < len; i++)
-    {
-        // Multiple of perLine means new or first line (with line offset).
+	// Process every byte in the data.
 
-        if ((i % perLine) == 0)
-        {
-            // Only print previous-line ASCII buffer for lines beyond first.
+	for (i = 0; i < len; i++)
+	{
+		// Multiple of perLine means new or first line (with line offset).
 
-            if (i != 0)
-                printf("  %s\n", buff);
+		if ((i % perLine) == 0)
+		{
+			// Only print previous-line ASCII buffer for lines beyond first.
 
-            // Output the offset of current line.
+			if (i != 0)
+				printf("  %s\n", buff);
 
-            printf("  %04x ", i);
-        }
+			// Output the offset of current line.
 
-        // Now the hex code for the specific character.
+			printf("  %04x ", i);
+		}
 
-        printf(" %02x", pc[i]);
+		// Now the hex code for the specific character.
 
-        // And buffer a printable ASCII character for later.
+		printf(" %02x", pc[i]);
 
-        if ((pc[i] < 0x20) || (pc[i] > 0x7e)) // isprint() may be better.
-            buff[i % perLine] = '.';
-        else
-            buff[i % perLine] = pc[i];
-        buff[(i % perLine) + 1] = '\0';
-    }
+		// And buffer a printable ASCII character for later.
 
-    // Pad out last line if not exactly perLine characters.
+		if ((pc[i] < 0x20) || (pc[i] > 0x7e)) // isprint() may be better.
+			buff[i % perLine] = '.';
+		else
+			buff[i % perLine] = pc[i];
+		buff[(i % perLine) + 1] = '\0';
+	}
 
-    while ((i % perLine) != 0)
-    {
-        printf("   ");
-        i++;
-    }
+	// Pad out last line if not exactly perLine characters.
 
-    // And print the final ASCII buffer.
+	while ((i % perLine) != 0)
+	{
+		printf("   ");
+		i++;
+	}
 
-    printf("  %s\n", buff);
+	// And print the final ASCII buffer.
+
+	printf("  %s\n", buff);
 }
 
 // void turtle_example()
--- src/TurtleAppWindow.hpp
+++ src/TurtleAppWindow.hpp
@@ -9,37 +9,39 @@
 #include <QtWidgets/QDockWidget>
 #include "TurtleCanvasWidget.hpp"
 
-class TurtleAppWindow : public QMainWindow
-{
-    Q_OBJECT
-public:
-    explicit TurtleAppWindow(QWidget *parent = nullptr);
+namespace turtle {
+	class TurtleAppWindow : public QMainWindow
+	{
+		Q_OBJECT
+	public:
+		explicit TurtleAppWindow(QWidget* parent = nullptr);
 
-signals:
+	signals:
 
 
-private:
-    QSize sizeHint() const;
+	private:
+		QSize sizeHint() const;
 
-    // helper functions
-    void create_actions();
-    void create_toolbar();
-    void create_menubar();
-    void create_canvas_widget();
-    void create_turtle_code_edit_widget();
+		// helper functions
+		void create_actions();
+		void create_toolbar();
+		void create_menubar();
+		void create_canvas_widget();
+		void create_turtle_code_edit_widget();
 
-    // Widgets
-    TurtleCanvasWidget* turtle_canvas;
-    QDockWidget* turtle_code_edit_dock;
-    QTextEdit *turtle_code_edit;
+		// Widgets
+		TurtleCanvasWidget* turtle_canvas;
+		QDockWidget* turtle_code_edit_dock;
+		QTextEdit* turtle_code_edit;
 
-    // Menu actions and toolbar
-    QAction* quit_action;
-    QAction* new_action;
-    QAction* open_action;
-    QAction* save_action;
-    QAction* save_as_action;
-    QToolBar* file_toolbar;
-};
+		// Menu actions and toolbar
+		QAction* quit_action;
+		QAction* new_action;
+		QAction* open_action;
+		QAction* save_action;
+		QAction* save_as_action;
+		QToolBar* file_toolbar;
+	};
+}
 
 #endif // MAINWINDOW_H
