d0d9b51f0fbdc8e01433bebe4e6cf744db7db7cb
Abhishek Mishra <abhishekmishra3@gmail.com>
2022-11-10 08:35:41 +0530

    drawing image taken from turtle skimage snapshot in every paint iteration

--- src/TurtleGLWidget.cpp
+++ src/TurtleGLWidget.cpp
@@ -1,15 +1,19 @@
 #include "TurtleGLWidget.hpp"
 
+#include "core/SkSurface.h"
 #include <QPainter>
 #include <QTimer>
 #include <QPaintEvent>
+#include <QImage>
+
+#include "Turtle.hpp"
 
 TurtleGLWidget::TurtleGLWidget(QWidget *parent)
     : QOpenGLWidget(parent),
       turtle{NULL}
 {
     elapsed = 0;
-    setFixedSize(300, 300);
+    setFixedSize(TURTLE_DEFAULT_CANVAS_WIDTH, TURTLE_DEFAULT_CANVAS_HEIGHT);
     setAutoFillBackground(false);
     background = QBrush(QColor(64, 32, 64));
 
@@ -32,18 +36,30 @@ void TurtleGLWidget::paintEvent(QPaintEvent *event)
     // helper->paint(&painter, event, elapsed);
 
     painter.fillRect(event->rect(), background);
-    painter.translate(150, 150);
-
-    painter.setPen(textPen);
-    painter.setFont(textFont);
     if (turtle == NULL) 
     {
+        painter.translate(150, 150);
+
+        painter.setPen(textPen);
+        painter.setFont(textFont);
         painter.drawText(QRect(-50, -50, 100, 100), Qt::AlignCenter, QStringLiteral("Yolo"));
     }
     else
     {
-        QString out_text = QString("Turtle -> ") + QString::fromStdString(turtle->getName());
-        painter.drawText(QRect(-100, -100, 150, 150), Qt::AlignCenter, out_text);
+        // QString out_text = QString("Turtle -> ") + QString::fromStdString(turtle->getName());
+        // painter.drawText(QRect(-100, -100, 150, 150), Qt::AlignCenter, out_text);
+        sk_sp<SkImage> img = turtle->getRasterSurface()->makeImageSnapshot();
+
+        SkImageInfo info = SkImageInfo::MakeN32Premul(img->width(), img->height());
+        std::vector<uint32_t> srcPixels;
+        const int rowBytes = img->width() * 4;
+        srcPixels.resize(img->height() * rowBytes);
+        SkPixmap pixmap(info, (const void *)&srcPixels.front(), rowBytes);
+        img->readPixels(pixmap, 0, 0);
+
+        QImage qImg = QImage((uchar*)&srcPixels.front(),img->width(), img->height(), QImage::Format_RGBA8888_Premultiplied);
+
+        painter.drawImage(QPointF(0, 0), qImg);
     }
     painter.end();
 }
