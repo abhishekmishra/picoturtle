f2068bf14f3a32e7dc5a4ad5c490654381efe608
Abhishek Mishra <abhishekmishra3@gmail.com>
2022-11-24 12:53:50 +0530

    using color type discovered at runtime rather than preprocessor macro to choose which QImage type to use

--- src/TurtleGLWidget.cpp
+++ src/TurtleGLWidget.cpp
@@ -58,35 +58,45 @@ void TurtleGLWidget::paintEvent(QPaintEvent *event)
 		// to debug the image color type of the given snapshot
 		if (!img.get()->isTextureBacked())
 		{
-			SkPixmap pixmap;
-			img->peekPixels(&pixmap);
-			SkColorType colorType = pixmap.colorType();
-			SkAlphaType alphaType = pixmap.alphaType();
+			SkPixmap peek_pixmap;
+			img->peekPixels(&peek_pixmap);
+			SkColorType colorType = peek_pixmap.colorType();
+			SkAlphaType alphaType = peek_pixmap.alphaType();
 			qDebug() << "color type = " << colorType << " alpha type = " << alphaType;
+
+			SkImageInfo info = SkImageInfo::MakeN32Premul(img->width(), img->height());
+			std::vector<uint32_t> srcPixels;
+			const int rowBytes = img->width() * 4;
+			srcPixels.resize(img->height() * rowBytes);
+			SkPixmap pixmap(info, (const void *)&srcPixels.front(), rowBytes);
+			img->readPixels(pixmap, 0, 0);
+
+			QImage qImg;
+
+			switch(colorType)
+			{
+				case kRGBA_8888_SkColorType:
+					qImg = QImage((uchar *)&srcPixels.front(), img->width(), img->height(), QImage::Format_RGBA8888);
+					break;
+
+				case kBGRA_8888_SkColorType:
+					qImg = QImage((uchar *)&srcPixels.front(), img->width(), img->height(), QImage::Format_ARGB32_Premultiplied);
+					break;
+
+				default:
+					qDebug() << "Not sure how to deal with this image color type";
+					break;
+
+			}
+			// qDebug() << "Image size is" << img->width() << "x" << img->height();
+			// qDebug() << "GLWidget size is" << size().width() << "x" << size().height();
+
+			painter.drawImage(QRect(0, 0, size().width(), size().height()), qImg);
 		}
 		else
 		{
 			qDebug() << "this is a texture on the GPU and can't be determined easily";
 		}
-
-		SkImageInfo info = SkImageInfo::MakeN32Premul(img->width(), img->height());
-		std::vector<uint32_t> srcPixels;
-		const int rowBytes = img->width() * 4;
-		srcPixels.resize(img->height() * rowBytes);
-		SkPixmap pixmap(info, (const void *)&srcPixels.front(), rowBytes);
-		img->readPixels(pixmap, 0, 0);
-
-		// qDebug() << "Image size is" << img->width() << "x" << img->height();
-		// qDebug() << "GLWidget size is" << size().width() << "x" << size().height();
-#if SK_PMCOLOR_BYTE_ORDER(B, G, R, A)
-		QImage qImg = QImage((uchar *)&srcPixels.front(), img->width(), img->height(), QImage::Format_ARGB32);
-#elif SK_PMCOLOR_BYTE_ORDER(R, G, B, A)
-		// TODO: learn which one to use rgba8888 or rgba32premul
-		QImage qImg = QImage((uchar *)&srcPixels.front(), img->width(), img->height(), QImage::Format_RGBA8888);
-#else
-		qDebug() << "Undefined byte order of sk color";
-#endif
-		painter.drawImage(QRect(0, 0, size().width(), size().height()), qImg);
 	}
 	painter.end();
 }
