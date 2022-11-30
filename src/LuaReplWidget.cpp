09a75ee8a0710841bb725b47b0f1276d164623ea
Abhishek Mishra <abhishekmishra3@gmail.com>
2022-11-30 12:34:38 +0530

    added line entry widget for entering lua lines

--- src/LuaReplWidget.cpp
+++ src/LuaReplWidget.cpp
@@ -1,12 +1,17 @@
 #include <QBoxLayout>
+#include <QHboxLayout>
 #include "LuaReplWidget.hpp"
+#include <QTextBlock>
 
 using namespace turtle;
 
+const std::string LuaReplWidget::LUA_REPL_PROMPT = "lua";
 
-LuaReplWidget::LuaReplWidget(QWidget* parent)
+LuaReplWidget::LuaReplWidget(QWidget *parent)
 {
-	lua_repl_edit = new QPlainTextEdit(this);
+	lua_repl_display = new QPlainTextEdit(this);
+	lua_repl_display->setReadOnly(true);
+	lua_repl_display->appendPlainText("############# Lua Session #############");
 
 	// Set the default monospace font for now
 	// TODO: perhaps include a decent open source font
@@ -14,9 +19,9 @@ LuaReplWidget::LuaReplWidget(QWidget* parent)
 	font.setPointSize(12);
 	QFontInfo info(font);
 	// qDebug() << font << info.family() << info.fixedPitch();
-	lua_repl_edit->setFont(font);
+	lua_repl_display->setFont(font);
 
-	QPalette p = lua_repl_edit->palette();
+	QPalette p = lua_repl_display->palette();
 
 	// set background colour
 	p.setColor(QPalette::Active, QPalette::Base, Qt::darkBlue);
@@ -26,11 +31,49 @@ LuaReplWidget::LuaReplWidget(QWidget* parent)
 	p.setColor(QPalette::Active, QPalette::Text, Qt::white);
 	p.setColor(QPalette::Inactive, QPalette::Text, Qt::white);
 
-	lua_repl_edit->setPalette(p);
+	lua_repl_display->setPalette(p);
 
-	lua_repl_edit->setMinimumHeight(150);
+	lua_repl_display->setMinimumHeight(150);
 
-	QVBoxLayout* vb_layout = new QVBoxLayout(this);
-	vb_layout->addWidget(lua_repl_edit);
+	// create line entry widgets
+	lua_entry = new QLineEdit(this);
+	lua_prompt = new QLabel(QString::fromStdString(LUA_REPL_PROMPT) + " >", this);
+
+	QHBoxLayout *lua_line_hbox = new QHBoxLayout(this);
+	lua_line_hbox->addWidget(lua_prompt);
+	lua_line_hbox->addWidget(lua_entry);
+	QWidget *lua_line_widget = new QWidget();
+	lua_line_widget->setLayout(lua_line_hbox);
+	lua_line_hbox->setContentsMargins(2, 2, 0, 0);
+
+	QVBoxLayout *vb_layout = new QVBoxLayout(this);
+	vb_layout->addWidget(lua_repl_display);
+	vb_layout->addWidget(lua_line_widget);
 	setLayout(vb_layout);
+
+	// actions on the line entry
+	connect(lua_entry, &QLineEdit::returnPressed, [=]()
+			{ auto text = lua_entry->text();
+			lua_repl_display->appendPlainText(text);
+			lua_entry->setText(""); });
 };
+
+/*
+** Check whether 'status' signals a syntax error and the error
+** message at the top of the stack ends with the above mark for
+** incomplete statements.
+*/
+int LuaReplWidget::incomplete(lua_State *L, int status)
+{
+	if (status == LUA_ERRSYNTAX)
+	{
+		size_t lmsg;
+		const char *msg = lua_tolstring(L, -1, &lmsg);
+		if (lmsg >= marklen && strcmp(msg + lmsg - marklen, EOFMARK) == 0)
+		{
+			lua_pop(L, 1);
+			return 1;
+		}
+	}
+	return 0; /* else... */
+}
--- src/LuaReplWidget.hpp
+++ src/LuaReplWidget.hpp
@@ -3,16 +3,45 @@
 #ifndef LUAREPLWIDGET_H
 #define LUAREPLWIDGET_H
 
+#include <string>
 #include <QWidget>
 #include <QPlainTextEdit>
+#include <QLineEdit>
+#include <QLabel>
 
+extern "C"
+{
+
+#include <lua.h>
+#include <lauxlib.h>
+#include <lualib.h>
+}
+
+/*
+** {==================================================================
+** Read-Eval-Print Loop (REPL)
+** ===================================================================
+*/
+
+#if !defined(LUA_PROMPT)
+#define LUA_PROMPT "> "
+#define LUA_PROMPT2 ">> "
+#endif
+
+#if !defined(LUA_MAXINPUT)
+#define LUA_MAXINPUT 512
+#endif
+
+/* mark in error messages for incomplete statements */
+#define EOFMARK "<eof>"
+#define marklen (sizeof(EOFMARK) / sizeof(char) - 1)
 
 namespace turtle
 {
 	/**
 	 * @brief Implements a Lua REPL for use as a docked widget
 	 * or a standalone widget if required later.
-	 * 
+	 *
 	 * The code in this implementation is heavily based on lua.c
 	 * in the Lua 5.4 source code.
 	 */
@@ -20,15 +49,17 @@ namespace turtle
 	{
 		Q_OBJECT
 
+		static const std::string LUA_REPL_PROMPT;
+
 	public:
-		LuaReplWidget(QWidget* parent = nullptr);
+		LuaReplWidget(QWidget *parent = nullptr);
 
 	private:
-		QPlainTextEdit* lua_repl_edit;
-
+		QPlainTextEdit *lua_repl_display;
+		QLineEdit *lua_entry;
+		QLabel *lua_prompt;
+		int incomplete(lua_State *L, int status);
 	};
-
 };
-
 
 #endif // LUAREPLWIDGET_H
