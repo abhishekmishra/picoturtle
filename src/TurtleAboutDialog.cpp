74c51c461308b0a73557c494192a061269ce45b2
Abhishek Mishra <abhishekmishra3@gmail.com>
2022-12-24 08:42:32 +0530

    bumped version

--- src/TurtleAboutDialog.cpp
+++ src/TurtleAboutDialog.cpp
@@ -5,31 +5,30 @@
 
 using namespace turtle;
 
-
-void enable_hyperlinks(QLabel* label)
+void enable_hyperlinks(QLabel *label)
 {
 	label->setTextFormat(Qt::RichText);
 	label->setTextInteractionFlags(Qt::TextBrowserInteraction);
 	label->setOpenExternalLinks(true);
 }
 
-TurtleAboutDialog::TurtleAboutDialog(QWidget* parent)
+TurtleAboutDialog::TurtleAboutDialog(QWidget *parent)
 {
 	setWindowTitle(tr("About PicoTurtle"));
 
 	// TODO: get actual version from cmake build.
-	QLabel* program_name = new QLabel("<a href='https://github.com/abhishekmishra/picoturtle'>PicoTurtle</a> v0.1.0-alpha is a Turtle programming environment.");
+	QLabel *program_name = new QLabel("<a href='https://github.com/abhishekmishra/picoturtle'>PicoTurtle</a> v0.2.0-alpha.0 is a Turtle programming environment.");
 	enable_hyperlinks(program_name);
 
-	QLabel* author_name = new QLabel("by Abhishek Mishra (<a href='https://github.com/abhishekmishra'>github.com/abhishekmishra</a>)");
+	QLabel *author_name = new QLabel("by Abhishek Mishra (<a href='https://github.com/abhishekmishra'>github.com/abhishekmishra</a>)");
 	enable_hyperlinks(author_name);
 
-	QLabel* license_brief = new QLabel("PicoTurtle license - <a href='https://raw.githubusercontent.com/abhishekmishra/picoturtle/main/LICENSE'>GNU General Public License v3.0 only</a>.");
+	QLabel *license_brief = new QLabel("PicoTurtle license - <a href='https://raw.githubusercontent.com/abhishekmishra/picoturtle/main/LICENSE'>GNU General Public License v3.0 only</a>.");
 	enable_hyperlinks(license_brief);
-	QLabel* license_brief_part2 = new QLabel("For full license notices including those of dependencies see the source code.");
-	QLabel* copyright_brief = new QLabel("PicoTurtle  Copyright (C) 2018-2022  Abhishek Mishra");
+	QLabel *license_brief_part2 = new QLabel("For full license notices including those of dependencies see the source code.");
+	QLabel *copyright_brief = new QLabel("PicoTurtle  Copyright (C) 2018-2022  Abhishek Mishra");
 
-	QDialogButtonBox* button_box = new QDialogButtonBox(QDialogButtonBox::Ok);
+	QDialogButtonBox *button_box = new QDialogButtonBox(QDialogButtonBox::Ok);
 
 	QVBoxLayout *vb_layout = new QVBoxLayout(this);
 
