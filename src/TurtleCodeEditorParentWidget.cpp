2829daf44de7ad4ced188d7053d544f76ee4b8a3
Abhishek Mishra <abhishekmishra3@gmail.com>
2023-01-02 08:21:54 +0530

    fixed segfault on last editor tab closed - by adding a new tab when the last one is being closed

--- src/TurtleCodeEditorParentWidget.cpp
+++ src/TurtleCodeEditorParentWidget.cpp
@@ -5,10 +5,10 @@
 
 using namespace turtle;
 
-TurtleCodeEditorParentWidget::TurtleCodeEditorParentWidget(TurtleLuaReplWidget* repl, QWidget* parent)
-	: tabs{ new QTabWidget(this) }, lua_repl(repl)
+TurtleCodeEditorParentWidget::TurtleCodeEditorParentWidget(TurtleLuaReplWidget *repl, QWidget *parent)
+	: tabs{new QTabWidget(this)}, lua_repl(repl)
 {
-	QVBoxLayout* vb_layout = new QVBoxLayout(this);
+	QVBoxLayout *vb_layout = new QVBoxLayout(this);
 	vb_layout->addWidget(tabs);
 	setLayout(vb_layout);
 
@@ -16,7 +16,7 @@ TurtleCodeEditorParentWidget::TurtleCodeEditorParentWidget(TurtleLuaReplWidget* 
 	tabs->setDocumentMode(true);
 
 	connect(tabs, &QTabWidget::tabCloseRequested, [=](int idx)
-		{
+			{
 			// get the handle to the editor widget
 			// then remove the tab
 			// and then delete the widget object.
@@ -56,13 +56,12 @@ TurtleCodeEditorParentWidget::TurtleCodeEditorParentWidget(TurtleLuaReplWidget* 
 	}
 	else {
 		delete_editor_and_tab_at_idx(idx);
-	}});
+	} });
 
 	connect(tabs, &QTabWidget::currentChanged, [=](int idx)
-		{
+			{
 			handle_current_tab_changed(idx);
-			emit current_tab_changed(idx); 
-		});
+			emit current_tab_changed(idx); });
 
 	new_file();
 }
@@ -75,40 +74,48 @@ void TurtleCodeEditorParentWidget::handle_current_tab_changed(int idx)
 {
 	for (int i = 0; i < tabs->count(); i++)
 	{
-		disconnect(((TurtleCodeEditorWidget*)tabs->widget(i))->get_editor(), &TurtleCodeEditorTextWidget::cursorPositionChanged, 0, 0);
+		disconnect(((TurtleCodeEditorWidget *)tabs->widget(i))->get_editor(), &TurtleCodeEditorTextWidget::cursorPositionChanged, 0, 0);
 	}
-	TurtleCodeEditorTextWidget* editor = ((TurtleCodeEditorWidget*)tabs->widget(idx))->get_editor();
-	connect(editor, &TurtleCodeEditorTextWidget::cursorPositionChanged, [=]() {
-		emit cursor_position_changed(editor->current_line(), editor->current_column());
-		});
+	TurtleCodeEditorTextWidget *editor = ((TurtleCodeEditorWidget *)tabs->widget(idx))->get_editor();
+	connect(editor, &TurtleCodeEditorTextWidget::cursorPositionChanged, [=]()
+			{ emit cursor_position_changed(editor->current_line(), editor->current_column()); });
 }
 
+// While deleting tab at index 0, if it is the last one,
+// Qt has an ugly segfault.
+// This code adds a new file tab if the last one is to be
+// deleted - to avoid this bug.
 void TurtleCodeEditorParentWidget::delete_editor_and_tab_at_idx(int idx)
 {
-	qDebug() << "Request to delete tab at index:" << idx;
-	TurtleCodeEditorWidget* editor_widget = (TurtleCodeEditorWidget*)tabs->widget(idx);
-	qDebug() << "Editor widget extracted";
+	if (idx == 0 && tabs->count() == 1)
+	{
+		// qDebug() << "Deleting last tab at index 0, so let's add one before deleting.";
+		new_file();
+	}
+	// qDebug() << "Request to delete tab at index:" << idx;
+	TurtleCodeEditorWidget *editor_widget = (TurtleCodeEditorWidget *)tabs->widget(idx);
+	// qDebug() << "Editor widget extracted";
 	tabs->removeTab(idx);
-	qDebug() << "Tab removed";
+	// qDebug() << "Tab removed";
 	delete editor_widget;
-	qDebug() << "Editor widget delete called.";
+	// qDebug() << "Editor widget delete called.";
 }
 
 void TurtleCodeEditorParentWidget::new_file()
 {
-	TurtleCodeEditorWidget* code_editor = new_code_editor();
+	TurtleCodeEditorWidget *code_editor = new_code_editor();
 	code_editor->new_file();
 }
 
-int TurtleCodeEditorParentWidget::open_file(const QString& file_path)
+int TurtleCodeEditorParentWidget::open_file(const QString &file_path)
 {
-	TurtleCodeEditorWidget* code_editor = new_code_editor();
+	TurtleCodeEditorWidget *code_editor = new_code_editor();
 	return code_editor->open_file(file_path);
 }
 
 int TurtleCodeEditorParentWidget::save_file()
 {
-	TurtleCodeEditorWidget* code_editor = get_current_editor_widget();
+	TurtleCodeEditorWidget *code_editor = get_current_editor_widget();
 	if (code_editor != NULL)
 	{
 		return code_editor->save_file();
@@ -116,9 +123,9 @@ int TurtleCodeEditorParentWidget::save_file()
 	return -1;
 }
 
-bool TurtleCodeEditorParentWidget::set_file_path(const QString& file_path, bool override_current_path)
+bool TurtleCodeEditorParentWidget::set_file_path(const QString &file_path, bool override_current_path)
 {
-	TurtleCodeEditorWidget* code_editor = get_current_editor_widget();
+	TurtleCodeEditorWidget *code_editor = get_current_editor_widget();
 	if (code_editor != NULL)
 	{
 		return code_editor->set_file_path(file_path, override_current_path);
@@ -128,7 +135,7 @@ bool TurtleCodeEditorParentWidget::set_file_path(const QString& file_path, bool 
 
 void TurtleCodeEditorParentWidget::run_file()
 {
-	TurtleCodeEditorWidget* code_editor = get_current_editor_widget();
+	TurtleCodeEditorWidget *code_editor = get_current_editor_widget();
 	if (code_editor != NULL)
 	{
 		return code_editor->run_file();
@@ -137,7 +144,7 @@ void TurtleCodeEditorParentWidget::run_file()
 
 bool TurtleCodeEditorParentWidget::has_file_path()
 {
-	TurtleCodeEditorWidget* code_editor = get_current_editor_widget();
+	TurtleCodeEditorWidget *code_editor = get_current_editor_widget();
 	if (code_editor != NULL)
 	{
 		return code_editor->has_file_path();
@@ -147,7 +154,7 @@ bool TurtleCodeEditorParentWidget::has_file_path()
 
 bool TurtleCodeEditorParentWidget::is_dirty()
 {
-	TurtleCodeEditorWidget* code_editor = get_current_editor_widget();
+	TurtleCodeEditorWidget *code_editor = get_current_editor_widget();
 	if (code_editor != NULL)
 	{
 		return code_editor->is_dirty();
@@ -156,7 +163,7 @@ bool TurtleCodeEditorParentWidget::is_dirty()
 }
 const QString TurtleCodeEditorParentWidget::get_file_name()
 {
-	TurtleCodeEditorWidget* code_editor = get_current_editor_widget();
+	TurtleCodeEditorWidget *code_editor = get_current_editor_widget();
 	if (code_editor != NULL)
 	{
 		return code_editor->get_file_name();
@@ -164,9 +171,9 @@ const QString TurtleCodeEditorParentWidget::get_file_name()
 	return QString();
 }
 
-const QString& TurtleCodeEditorParentWidget::get_file_path()
+const QString &TurtleCodeEditorParentWidget::get_file_path()
 {
-	TurtleCodeEditorWidget* code_editor = get_current_editor_widget();
+	TurtleCodeEditorWidget *code_editor = get_current_editor_widget();
 	if (code_editor != NULL)
 	{
 		return code_editor->get_file_path();
@@ -175,10 +182,10 @@ const QString& TurtleCodeEditorParentWidget::get_file_path()
 	return *(new QString());
 }
 
-TurtleCodeEditorWidget* TurtleCodeEditorParentWidget::get_current_editor_widget()
+TurtleCodeEditorWidget *TurtleCodeEditorParentWidget::get_current_editor_widget()
 {
 	// qDebug() << "current index is ->" << tabs->currentIndex();
-	return (TurtleCodeEditorWidget*)tabs->currentWidget();
+	return (TurtleCodeEditorWidget *)tabs->currentWidget();
 }
 
 /**
@@ -186,23 +193,23 @@ TurtleCodeEditorWidget* TurtleCodeEditorParentWidget::get_current_editor_widget(
  * The newly created widget is also made the current tab.
  * All of the code editor widget's signals are connected to this widget.
  */
-TurtleCodeEditorWidget* TurtleCodeEditorParentWidget::new_code_editor()
+TurtleCodeEditorWidget *TurtleCodeEditorParentWidget::new_code_editor()
 {
-	TurtleCodeEditorWidget* code_editor = new TurtleCodeEditorWidget(lua_repl, this);
+	TurtleCodeEditorWidget *code_editor = new TurtleCodeEditorWidget(lua_repl, this);
 	tabs->addTab(code_editor, code_editor->get_file_name());
 	tabs->setCurrentWidget(code_editor);
 	code_editor->layout()->setContentsMargins(0, 0, 0, 0);
 
 	connect(code_editor, &TurtleCodeEditorWidget::new_file_created, [=]()
-		{ emit new_file_created(); });
+			{ emit new_file_created(); });
 	connect(code_editor, &TurtleCodeEditorWidget::file_opened, [=]()
-		{ emit file_opened(); });
+			{ emit file_opened(); });
 	connect(code_editor, &TurtleCodeEditorWidget::file_saved, [=]()
-		{ emit file_saved(); });
+			{ emit file_saved(); });
 	connect(code_editor, &TurtleCodeEditorWidget::turtle_run_complete, [=](const int error_code)
-		{ emit turtle_run_complete(error_code); });
+			{ emit turtle_run_complete(error_code); });
 	connect(code_editor, &TurtleCodeEditorWidget::file_path_changed, [=]()
-		{
+			{
 			int idx = tabs->indexOf(code_editor);
 	QString tab_name = code_editor->get_file_name();
 	if (code_editor->is_dirty()) {
@@ -212,7 +219,7 @@ TurtleCodeEditorWidget* TurtleCodeEditorParentWidget::new_code_editor()
 	emit file_path_changed(); });
 
 	connect(code_editor, &TurtleCodeEditorWidget::file_modified_changed, [=](bool modified)
-		{
+			{
 			int idx = tabs->indexOf(code_editor);
 	QString tab_name = code_editor->get_file_name();
 	if (code_editor->is_dirty()) {
