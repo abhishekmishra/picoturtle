be106f6252470982e6ef4db84ae201e990b2c5c6
Abhishek Mishra <abhishekmishra3@gmail.com>
2023-02-08 08:55:56 +0530

    implemented gif creation util function

--- src/GifUtil.c
+++ src/GifUtil.c
@@ -1,7 +1,50 @@
 #include "GifUtil.h"
 #include <wand/magick_wand.h>
+#include <string.h>
+#include <stdio.h>
 
-int pt_make_gif(char *img_filenames[], int num_images, char *output_filename)
+void init_gif_util()
 {
+    InitializeMagick(NULL);
+}
+
+void exit_gif_util()
+{
+    DestroyMagick();
+}
+
+int make_gif_from_images(char *img_filenames[], int num_images, 
+    char *output_filename)
+{
+    if(img_filenames == NULL)
+    {
+        printf("Error:: Image list for gif is empty. Gif creation aborted!");
+        return -1;
+    }
+
+    if (num_images == 0)
+    {
+        printf("Error:: No images in image list for gif. \
+            Gif creation aborted!");
+        return -1;
+    }
+
+    if (output_filename == NULL || strlen(output_filename) == 0)
+    {
+        printf("Error:: Gif filename is null. \
+            Gif creation aborted!");
+        return -1;
+    }
+
+    MagickWand * wand;
+    wand = NewMagickWand();
+
+    for (int i = 0; i < num_images; i++)
+    {
+        MagickReadImage(wand, img_filenames[i]);
+    }
+
+    MagickWriteImages(wand, output_filename, MagickTrue);
+
     return 0;
 }
--- src/GifUtil.h
+++ src/GifUtil.h
@@ -1,6 +1,11 @@
 #ifndef __PICOTURTLE_GIF_UTIL__
 #define __PICOTURTLE_GIF_UTIL__
 
-int pt_make_gif(char *img_filenames[], int num_images, char *output_filename);
+void init_gif_util();
+
+void exit_gif_util();
+
+int make_gif_from_images(char *img_filenames[], int num_images, 
+    char *output_filename);
 
 #endif //__PICOTURTLE_GIF_UTIL__
