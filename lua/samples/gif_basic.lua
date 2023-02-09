d9b904827089c0116d4567cbd6284d78712488ce
Abhishek Mishra <abhishekmishra3@gmail.com>
2023-02-09 19:32:00 +0530

    more frames in gif

--- lua/samples/gif_basic.lua
+++ lua/samples/gif_basic.lua
@@ -7,15 +7,17 @@
 local pt = require'picoturtle'
 local t = t or pt.new()
 
+t:penup()
+t:setpos(50, 50)
 t:pendown()
 t:penwidth(10)
 t:pencolor('red')
 
-local num = 5
+local num = 9
 
 local snaps = {}
 
-for i = 1, 5, 1 do
+for i = 1, num, 1 do
     t:fd((num - i) * 100)
     t:rt(90)
     local imgname = 'out/basic' .. tostring(i) .. '.png'
