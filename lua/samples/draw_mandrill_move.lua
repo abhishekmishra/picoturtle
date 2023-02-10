e9987522359a159d815a7102fdb8fa2c41b6169a
Abhishek Mishra <abhishekmishra3@gmail.com>
2023-02-10 09:22:35 +0530

    mandrill move

---
+++ lua/samples/draw_mandrill_move.lua
@@ -0,0 +1,17 @@
+-- draw_mandrill.lua
+-- Author: Abhishek Mishra
+-- Date: 10/03/2023
+--
+-- Draws the mandrill png test image on the canvas
+--
+
+local t = t or require'picoturtle'.new()
+
+t:pu()
+
+for i = 1, 3, 1 do
+	t:setpos(100, i * 100)
+	t:clear()
+	t:drawimg("res/images/mandrill.png")
+	t:delay(100)
+end
