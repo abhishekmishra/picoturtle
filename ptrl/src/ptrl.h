953d9ce2e542b9508efae88bffcebc9bc973a21f
Abhishek Mishra <abhishekmishra3@gmail.com>
2025-05-30 16:56:10 +0530

    renamed for consistency

--- ptrl/src/ptrl.h
+++
@@ -1,6 +0,0 @@
-#ifndef ___PTRL_H___
-#define ___PTRL_H___
-
-#include "turtle.h"
-
-#endif // ___PTRL_H___
---
+++ ptrl/src/ptrtl.h
@@ -0,0 +1,6 @@
+#ifndef ___PTRTL_H___
+#define ___PTRTL_H___
+
+#include "turtle.h"
+
+#endif // ___PTRTL_H___
